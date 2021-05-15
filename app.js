802607697a7abbd94fc86d4ae786e33cc91ff3f3
Yash <yashgarg0905@gmail.com>
2021-05-16 00:19:41 +0530

    Updated

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const path = require('path');
 const express = require('express');
-const dotenv = require('dotenv');
+const dotenv = require('dotenv'); //This module loads environment variables from a .env file that you create and adds them to the process.env object that is made available to the application.
 const morgan = require('morgan'); //It simplifies the process of logging requests to your application
-const connectDB = require('./config/db')
-
+const exphbs = require('express-handlebars');// To set up template engines
+const connectDB = require('./config/db');
 //Load Config
 dotenv.config({path: './config/config.env'});
 
@@ -10,13 +11,24 @@ connectDB()
 
 const app = express();
 
-if(process.env.NPM_ENV === 'development'){
+//Logging
+if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+//HandleBars
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+//Routers
+app.use('/', require('./routes/index'));
+
+//Static folder: public folder is use by anyone no need to define path
+app.use(express.static(path.join(__dirname,'public'))); 
+
+
+
 const PORT = process.env.PORT || 3000
-
-
 app.listen(
     PORT,
     console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)    
