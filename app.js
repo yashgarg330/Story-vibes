0bcea175e0948f9d16529233728fedf3b1732f9b
Yash <yashgarg0905@gmail.com>
2021-05-19 01:52:54 +0530

    update

--- app.js
+++ app.js
@@ -1,52 +1,56 @@
-const path = require('path');
-const express = require('express');
-const dotenv = require('dotenv'); //This module loads environment variables from a .env file that you create and adds them to the process.env object that is made available to the application.
-const morgan = require('morgan'); //It simplifies the process of logging(console.log,put,get request) requests to your application
-const exphbs = require('express-handlebars');// To set up template engines
-const passport = require('passport');
-const session = require('express-session'); //You assign the client an ID and it makes all further requests using that ID. Information associated with the client is stored on the server linked to this ID.
-const connectDB = require('./config/db');
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv"); //This module loads environment variables from a .env file that you create and adds them to the process.env object that is made available to the application.
+const morgan = require("morgan"); //It simplifies the process of logging(console.log,put,get request) requests to your application
+const exphbs = require("express-handlebars"); // To set up template engines
+const passport = require("passport");
+const session = require("express-session"); //You assign the client an ID and it makes all further requests using that ID. Information associated with the client is stored on the server linked to this ID.
+const MongoStore = require("connect-mongo");
+const connectDB = require("./config/db");
 
 //Load Config
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 
 //passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-connectDB()
+connectDB();
 
 const app = express();
 
 //Logging
-if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //HandleBars
-app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 //Session
-app.use(session({
-    secret: 'keyboard cat',
+app.use(
+  session({
+    secret: "keyboard cat",
     resave: false,
-    saveUninitialized: false, //Dont create a session until its stored 
-  }))
+    saveUninitialized: false, //Dont create a session until its stored
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URI }),
+  })
+);
 
 //Passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
 //Static folder: public folder is use by anyone no need to define path
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
-app.use('/', require('./routes/index'));
-app.use('/auth', require('./routes/auth'));
+app.use("/", require("./routes/index"));
+app.use("/auth", require("./routes/auth"));
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(
-    PORT,
-    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,24 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
+const { ensureAuth, ensureGuest } = require("../middleware/auth");
 // @desc Login/Landing Page
 // @route GET /
-router.get('/' , (req,res) => {
-    res.render('login',{
-        layout: 'login',
-    });
-})
+router.get("/", ensureGuest, (req, res) => {
+  res.render("login", {
+    layout: "login",
+  });
+});
 // @desc DashBoard
 // @route GET /dashboard
-router.get('/dashboard' , (req,res) => {
-    res.render('dashboard');
-})
- 
+router.get("/dashboard", ensureAuth, (req, res) => {
+  res.render("dashboard", {
+    name: req.user.firstName,
+  });
+});
 
 module.exports = router;
+
+//Aim - ensureAuth ke fir se login karne ke jarurut nah pade
+//Aim - ensureGuest ensures that guest login page pe he rahe even if try to go dashboard
+//Problem - agr server band karke frr se chalenge toh already login wale kick out hojaeyge
+//Solution - session store karna padega database meh so agr server restart bhi ho toh login user logout nah ho automatically
